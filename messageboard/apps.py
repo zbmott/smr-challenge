003361430b1d5a19ed185cd9ec50e2b878cadb3c
Zachary B. Mott <zmott@fantasyflightgames.com>
2017-04-14 22:35:54 -0500

    Initialize messageboard backend application.

---
+++ messageboard/apps.py
@@ -0,0 +1,10 @@
+from __future__ import unicode_literals
+
+from django.apps import AppConfig
+
+
+class MessageboardConfig(AppConfig):
+    name = 'messageboard'
+
+    def ready(self):
+        pass
